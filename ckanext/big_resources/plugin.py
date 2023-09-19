1dcb6c113d2cbab4f244490647ea7ff9c8ee92b4
blagoja <blagojabozinovski@gmail.com>
2023-09-19 19:53:56 +0200

    Initial commit

---
+++ ckanext/big_resources/plugin.py
@@ -0,0 +1,70 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+import ckanext.big_resources.views as views
+from ckan.lib.uploader import ResourceUpload as DefaultResourceUpload
+from ckan.lib.uploader import _copy_file
+import os
+import ckan.logic as logic
+
+
+class BigResourcesPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IUploader, inherit=True)
+
+    def get_resource_uploader(self, data_dict):
+        return ResourceUpload(data_dict)
+    
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic',
+            'big_resources')
+        
+
+class ResourceUpload(DefaultResourceUpload):
+
+    def upload(self, id, max_size=10):
+        '''Actually upload the file.
+
+        :returns: ``'file uploaded'`` if a new file was successfully uploaded
+            (whether it overwrote a previously uploaded file or not),
+            ``'file deleted'`` if an existing uploaded file was deleted,
+            or ``None`` if nothing changed
+        :rtype: ``string`` or ``None``
+
+        '''
+        if not self.storage_path:
+            return
+
+        # Get directory and filepath on the system
+        # where the file for this resource will be stored
+        directory = self.get_directory(id)
+        filepath = self.get_path(id)
+
+        # If a filename has been provided (a file is being uploaded)
+        # we write it to the filepath (and overwrite it if it already
+        # exists). This way the uploaded file will always be stored
+        # in the same location
+        breakpoint()
+        if self.filename:
+            try:
+                os.makedirs(directory)
+            except OSError as e:
+                # errno 17 is file already exists
+                if e.errno != 17:
+                    raise
+            tmp_filepath = filepath + '~'
+            with open(tmp_filepath, 'wb+') as output_file:
+                try:
+                    _copy_file(self.upload_file, output_file, max_size)
+                except logic.ValidationError:
+                    os.remove(tmp_filepath)
+                    raise
+                finally:
+                    self.upload_file.close()
+            os.rename(tmp_filepath, filepath)
+            return
+        
+    
